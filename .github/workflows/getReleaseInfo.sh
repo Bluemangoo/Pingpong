2f13659f0ebcaafd8ec68966aead7917caed6bb6
Bluemangoo <chenfy2006@qq.com>
2024-04-30 10:51:44 +0800

    Create getReleaseInfo.sh

---
+++ .github/workflows/getReleaseInfo.sh
@@ -0,0 +1,5 @@
+version=$(grep version Cargo.toml -m 1 | cut -d'=' -f2 | tr -d "\r" | tr -d ' ' | tr -d '"' | tr -d "'")
+release_info=release.md
+echo "## Changes" > $release_info
+number=$(git log --oneline master ^`git describe --tags --abbrev=0` | wc -l)
+echo "$(git log --pretty='> [%h] %s' -$number)" >> $release_info
